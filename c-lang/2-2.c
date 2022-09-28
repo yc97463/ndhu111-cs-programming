8eee84e1081738d5ce420032bb46bf65144b1625
yc97463 <hi@imych.one>
2022-09-28 11:27:10 +0800

    feat(cLang/2-2): basic written

--- c-lang/2-2.c
+++ c-lang/2-2.c
@@ -6,7 +6,8 @@ int main()
     scanf("%f", &a);
     float b = 0;
     int c = a * 100;
-    if (a * 100 - c >= 0.5)
+    // printf("a*100: %f\nc: %d\na*100-c: %f\n", a * 100, c, a * 100 - c);
+    if ((a * 100 - c) >= 0.5)
     {
         b = a + 0.01;
     }
@@ -14,6 +15,12 @@ int main()
     {
         b = a;
     }
-    printf("%.2f\n%.2f\n", b, a);
+    // printf("b: %f\n", b);
+    int oa = a * 100;
+    float oaa = oa / 100.0;
+    int ob = b * 100;
+    float obb = ob / 100.0;
+    printf("%.2f\n%.2f\n", obb, oaa);
+    // printf("%f\n%f\n", b, a);
     return 0;
 }
