f4569f87e928f365c068ef9e5b335d812ccb0de5
yc97463 <hi@imych.one>
2023-03-01 12:37:08 +0800

    feat(cpp/main): main world

---
+++ cpp/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int result;
+    int a = 2;
+    int b = 3;
+    result = a + b > 5 ? 6 : 9;
+    cout << result << endl;
+    return 0;
+}
